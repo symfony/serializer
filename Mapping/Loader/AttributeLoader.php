711ffd64ce348e2bc649c8afb267f2bc21bce46a
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-01-06 16:04:58 +0100

    [Serializer] Fix #[Ignore] on same-name properties <-> methods

--- Mapping/Loader/AttributeLoader.php
+++ Mapping/Loader/AttributeLoader.php
@@ -186,9 +186,7 @@ class AttributeLoader implements LoaderInterface
 
                     $attributeMetadata->setSerializedPath($annotation->getSerializedPath());
                 } elseif ($annotation instanceof Ignore) {
-                    if ($accessorOrMutator && !$hasProperty) {
-                        $attributeMetadata->setIgnore(true);
-                    }
+                    $attributeMetadata->setIgnore(true);
                 } elseif ($annotation instanceof Context) {
                     if (!$accessorOrMutator && !$hasProperty) {
                         throw new MappingException(\sprintf('Context on "%s::%s()" cannot be added. Context can only be added on methods beginning with "get", "is", "has", "can" or "set".', $className, $method->name));
